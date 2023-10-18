e5b1764b2b09c60e73489f51ed31d6448ac552e7
GOST_Playz <gundasathvik2914@gmail.com>
2023-10-17 22:16:12 -0500

    Updated Bot
    New update to the bot

--- dfsbot.py
+++ dfsbot.py
@@ -9,6 +9,10 @@ import random
 x = 0.5
 y = 0.5
 
+# target in-tile position 
+tile_x = 0.5
+tile_y = 0.5
+
 # last tile "reached" (i.e., being close enough to center)
 ty = -1
 tx = -1
@@ -43,7 +47,7 @@ while True:
       # print("comment now at: %s %s" % (x,y), flush=True)
       # update our latest tile reached once we are firmly on the inside of the tile
       if ((int(x) != tx or int(y) != ty) and
-          ((x-(int(x)+0.5))**2 + (y-(int(y)+0.5))**2)**0.5 < 0.2):
+          ((x-(int(x)+tile_x))**2 + (y-(int(y)+tile_y))**2)**0.5 < 0.2):
         tx = int(x)
         ty = int(y)
         if plan == []:
@@ -52,7 +56,7 @@ while True:
         #print("comment now at tile: %s %s" % (tx,ty), flush=True)
     elif obs[0] == "wall":
       #print("comment wall: %s %s %s %s" % (obs[1],obs[2],obs[3],obs[4]), flush=True)
-      # ensure every wall we see is tracked in our walls set
+      # ensure evetile_y wall we see is tracked in our walls set
       x0 = int(float(obs[1]))
       y0 = int(float(obs[2]))
       x1 = int(float(obs[3]))
@@ -81,24 +85,43 @@ while True:
     # assumes sufficient sense data, but that may not strictly be true  
     if len(seen) > 0 and (tx,ty+1) not in dead|seen and (tx,ty+1,tx+1,ty+1) not in walls:
       plan.append((tx,ty+1))  # move down
+      tile_x = 0.5
+      tile_y = 0.2
     elif len(seen) > 0 and (tx+1,ty) not in dead|seen and (tx+1,ty,tx+1,ty+1) not in walls:
       plan.append((tx+1,ty))  # move right
+      tile_x = 0.2
+      tile_y = 0.5
     elif len(seen) > 0 and (tx,ty-1) not in dead|seen and (tx,ty,tx+1,ty) not in walls:
       plan.append((tx,ty-1))  # move up
+      tile_x = 0.5
+      tile_y = 0.8
     elif len(seen) > 0 and (tx-1,ty) not in dead|seen and (tx,ty,tx,ty+1) not in walls:
       plan.append((tx-1,ty))  # move left
+      tile_x = 0.8
+      tile_y = 0.5
     else:
     # if we cannot advance or are not pathing currently, backtrack
       dead |= {(tx,ty)}
       plan = plan[:-1]
+      tile_x = 0.5
+      tile_y = 0.5
       # backtrack further, in one command, if we're
       #   returning to start AND its in a straight line:
       #while seen == set() and (plan[-1][0] == tx or plan[-1][1] == ty):
       #  plan = plan[:-1]
       
     # issue a command for the latest plan
-    print("toward %s %s" % (plan[-1][0]+0.5, plan[-1][1]+0.5), flush=True)
+    print("toward %s %s" % (plan[-1][0]+tile_x, plan[-1][1]+tile_y), flush=True)
   
   print("", flush=True)
   time.sleep(0.125)
+  
+# trying to enhance the dfsbot and make bot move to the edge of the tile : 
+# e.g: start         pos = 0.5 , 0.5
+#      move right    pos = 1.2 , 0.5
+#      move down     pos = 1.5 , 1.2
+
+#  when back track just move back to center of tile to avoid extra calculation.
+
+
   
